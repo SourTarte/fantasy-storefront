5b31bab43bd831673df78c9cf130c7e10742f1c2
SourTarte <dioncmcmillan@gmail.com>
2025-09-30 11:47:53 +0100

    fix: product details showing on cards

--- shop/models.py
+++ shop/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
         (
                 ('bow', 'Bow'),
                 ('crossbow', 'Crossbow'),
+                ('exotic', 'Exotic'),
             ),
         ),
         (
